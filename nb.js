d6f5e8849ed9ffe3c96a4256d2f0d17ed80186f6
Sadat <sadat.arefin98@gmail.com>
2020-09-16 02:22:52 +0600

    c

---
+++ nb.js
@@ -0,0 +1,9 @@
+console.log("Welcome to node js")
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
